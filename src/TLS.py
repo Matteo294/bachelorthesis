a731b95eb544d0647f6b0776f2df405c620b6432
Matteo294 <zmatteo.29@gmail.com>
2021-05-06 14:29:46 +0200

    Ramsey's criteria started

---
+++ src/TLS.py
@@ -0,0 +1,28 @@
+import numpy as np 
+from matplotlib import pyplot as plt 
+import matplotlib
+matplotlib.rcParams['mathtext.fontset'] = 'stix'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+
+def T(E, N=1):
+    return 1/np.log(((1-E)/E))
+
+def S(E):
+    return np.log(1/(1-E)) + E*np.log((1-E)/E)
+
+Evals1 = np.linspace(0.49, 0.49999, 1000)
+Evals2 = np.linspace(0.50001, 0.51, 1000)
+
+plt.plot(Evals1, T(Evals1), 'black', linewidth=2.0)
+plt.plot(Evals2, T(Evals2), 'black', linewidth=2.0)
+plt.xlabel(r'$E/n\epsilon$', fontsize=16)
+plt.ylabel(r'$\epsilon T/k_b$', fontsize=16)
+ax = plt.gca()
+ax.set_xticks([0.49, 0.5, 0.51])
+ax.set_yticks([0])
+ax.tick_params(axis='both', direction='in', bottom=True, top=False, left=True, right=False, labelbottom=True, labelleft=True)
+plt.show()
+
+plt.plot(Evals1, S(Evals1))
+plt.plot(Evals2, S(Evals2))
+plt.show()
